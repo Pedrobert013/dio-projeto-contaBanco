8d413d21bfb740a4ff502b38573fa917b78d383d
Pedro <pedbertasso@gmail.com>
2025-02-23 18:39:50 -0300

    projeto fianl com comentarios

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+        // Criando uma instancia da classe Scanner()
         Scanner scan = new Scanner(System.in);
 
         String nomeCliente;
